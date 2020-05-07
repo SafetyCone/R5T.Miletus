c96d576037b417e3fdc49f72e01317cfe2f6192a
MinexAutomation <minex.automation@gmail.com>
2020-05-07 12:13:45 -0700

    Added R5T.Miletus.Common library project.

---
+++ source/R5T.Miletus.Common/Code/Services/Implementations/SolutionDirectoryPathProvider.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using R5T.Karlstad;
+using R5T.Macommania;
+using R5T.Ujung;
+
+
+namespace R5T.Miletus.Common
+{
+    public class SolutionDirectoryPathProvider : ISolutionDirectoryPathProvider
+    {
+        private IExecutableFileDirectoryPathProvider ExecutableFileDirectoryPathProvider { get; }
+        private ISolutionAndProjectFileSystemConvention SolutionAndProjectFileSystemConventions { get; }
+
+
+        public SolutionDirectoryPathProvider(
+            IExecutableFileDirectoryPathProvider executableFileDirectoryPathProvider,
+            ISolutionAndProjectFileSystemConvention solutionAndProjectFileSystemConventions)
+        {
+            this.ExecutableFileDirectoryPathProvider = executableFileDirectoryPathProvider;
+            this.SolutionAndProjectFileSystemConventions = solutionAndProjectFileSystemConventions;
+        }
+
+        public string GetSolutionDirectoryPath()
+        {
+            var executableFileDirectoryPath = this.ExecutableFileDirectoryPathProvider.GetExecutableFileDirectoryPath();
+
+            var solutionDirectoryPath = this.SolutionAndProjectFileSystemConventions.GetSolutionDirectoryPathFromExecutableFileDirectoryPath(executableFileDirectoryPath);
+            return solutionDirectoryPath;
+        }
+    }
+}
---
+++ source/R5T.Miletus.Common/Code/Services/Implementations/StandardSolutionAndProjectFileSystemConvention.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using R5T.Karlstad;
+using R5T.Lombardy;
+
+
+namespace R5T.Miletus.Common
+{
+    // Standard is based on ..\R5T.Capua.Console\source\ΩMaintenance\bin\Debug\netcoreapp2.2
+    class StandardSolutionAndProjectFileSystemConvention : ISolutionAndProjectFileSystemConvention
+    {
+        private IStringlyTypedPathOperator StringlyTypedPathOperator { get; }
+
+
+        public StandardSolutionAndProjectFileSystemConvention(
+            IStringlyTypedPathOperator stringlyTypedPathOperator)
+        {
+            this.StringlyTypedPathOperator = stringlyTypedPathOperator;
+        }
+
+        public string GetSolutionDirectoryPathFromExecutableFileDirectoryPath(string executableFileDirectoryPath)
+        {
+            var ensuredExecutableFileDirectoryPath = this.StringlyTypedPathOperator.EnsureDirectoryPathIsDirectoryIndicated(executableFileDirectoryPath);
+
+            var buildConfigurationDirectoryPath = this.StringlyTypedPathOperator.GetParentDirectoryPathForDirectoryPath(ensuredExecutableFileDirectoryPath); // Debug
+            var binariesOutputDirectoryPath = this.StringlyTypedPathOperator.GetParentDirectoryPathForDirectoryPath(buildConfigurationDirectoryPath); // bin
+            var projectDirectoryPath = this.StringlyTypedPathOperator.GetParentDirectoryPathForDirectoryPath(binariesOutputDirectoryPath); // ΩMaintenance
+            var solutionDirectoryPath = this.StringlyTypedPathOperator.GetParentDirectoryPathForDirectoryPath(projectDirectoryPath); // source
+            return solutionDirectoryPath;
+        }
+    }
+}
