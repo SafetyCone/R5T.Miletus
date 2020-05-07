7af7ef8f4591da940cd7f694f387f61ac13adcf0
MinexAutomation <minex.automation@gmail.com>
2020-05-07 11:44:56 -0700

    Added Configure static class that uses static-value project-related name service implementations.

---
+++ source/R5T.Miletus.Configuration/Code/Classes/Configure.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using R5T.Palembang.Default;
+
+
+namespace R5T.Miletus.Configuration
+{
+    public static class Configure
+    {
+        public static void ConfigureProjectSpecificValues(string projectName, string buildConfigurationName, string targetFrameworkName)
+        {
+            StaticValueProjectNameProvider.ProjectName = projectName;
+            StaticValueBuildConfigurationNameProvider.BuildConfigurationName = buildConfigurationName;
+            StaticValueTargetFrameworkNameProvider.TargetFrameworkName = targetFrameworkName;
+        }
+    }
+}
